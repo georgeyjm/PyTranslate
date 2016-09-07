9138532fce6b4d198ef273a1587182f61036b990
George Yu <dumpling_yu@163.com>
2016-09-07 16:30:27 +0800

    Update PyTranslate to 2.0

--- PyTranslate.py
+++ PyTranslate.py
@@ -1,23 +1,20 @@
-# PyTranslate 1.0
+# PyTranslate
+# 2.0
 
-import codecs
-import time
-from math import *
+from codecs import open
+from time import time
 
-#Setting the Master Password
-masterPwd = int(pow(int(fabs(factorial(acos(1))-sqrt(4)+14641**(1/4)-tan(radians(45))*tan(pi/4)-acos(-0.5)*180/pi)+fmod(198279793,22031087)),(1/sqrt(sqrt(sqrt(sqrt(sqrt(4294967296))))))))
-j
 # Opens the data file, reads the data file, then splits word by word
 print('Importing the dictionary file...')
-startTime = time.clock()
-file = codecs.open('Dictionaries/dictionary2.txt', 'r+', 'utf-8')
-data = (file.read()).split('\n')
+startTime = time()
+file = open('Dictionaries/dictionary.txt', 'r+', 'utf-8')
+data = file.readlines()
 # Process every words one by one
 dictionary = {}
 for i in data:
     dictionary[(i.split(':'))[0]] = (i.split(':')[1]) # Adding keys into the dictionary
-endTime = time.clock()
-print('Finished! Time: %s milliseconds'%(str((endTime-startTime)*1000))[:4])
+endTime = time()
+print('Finished! Time: %.2f milliseconds'%((endTime-startTime)*1000))
 print('Dictionary has in total of %d words.'%len(data))
 
 # Main Program
@@ -27,30 +24,36 @@ while True:
     #Translate
     if option == '1':
         sentence = input('\nPlease input an English sentence: ')
-        splited = sentence.split()
+        wordOrderTmp = []
 
-        for i in splited:
-            if i.lower() in dictionary:
-                print(dictionary[i.lower()],end='')
-            else:
-                print(i,end=' ')
+        for i in sentence.split():
+            i = i.lower()
+            try:
+                if not dictionary[i]:
+                    continue
+                if '*' in dictionary[i]:
+                    wordOrderTmp = dictionary[i].split('*')
+                    print(wordOrderTmp[0], end='')
+                    continue
+                else:
+                    print(dictionary[i], end='')
+                if wordOrderTmp:
+                    print(wordOrderTmp[1], end='')
+                    wordOrderTmp = []
+            except Exception:
+                print(i, end=' ')
         print()
 
     #Add new word
     elif option == '2':
-        inputPwd = input('\nInput the Master Password in order to access this: ')
-        if int(inputPwd) == masterPwd:
-            print('Password Correct!')
-            english = input('\nInput the English word: ')
-            if english in dictionary:
-                print('\nWord exists!')
-                print('Current meaning: %s'%dictionary[english.lower()])
-                continue
-            chinese = input('Input the Chinese translation: ')
-            file.write(('\n' + english.lower() + ':' + chinese))
-            print('Successfully added \'%s\' into the dictionary'%english)
-        else:
-            print('Incorrect Password!')
+        english = input('\nInput the English word: ')
+        if english in dictionary:
+            print('\nWord exists!')
+            print('Current meaning: %s'%dictionary[english.lower()])
+            continue
+        chinese = input('Input the Chinese translation: ')
+        file.write(('\n' + english.lower() + ':' + chinese))
+        print('Successfully added \'%s\' into the dictionary'%english)
 
     #Save and exit
     elif option == '3':
@@ -59,4 +62,4 @@ while True:
 
     #Input Fail
     else:
-        print('\nBe Sensible!')
+        print('\nInvalid Input!')
